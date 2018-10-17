42d32b63b594f5f985922aa942ace73188ec9e7e
ninjayoshi <tim470773@gmail.com>
2018-10-17 19:55:31 -0400

    Created Event interface.

---
+++ Saturn - Android/app/src/test/java/utoronto/saturn/TestExistence.java
@@ -0,0 +1,31 @@
+package utoronto.saturn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestExistence {
+    private static final String PATH = "utoronto.saturn.";
+
+    private void assertClassExists(String path) {
+        try {
+            Class.forName(path);
+        } catch (ClassNotFoundException e) {
+            Assert.fail(path + " does not exist!");
+        }
+    }
+
+    @Test
+    public void checkEventManagerExists() {
+        assertClassExists(PATH + "EventManager");
+    }
+
+    @Test
+    public void checkLocalEventManagerExists() {
+        assertClassExists(PATH + "LocalEventManager");
+    }
+
+    @Test
+    public void checkEventExists() {
+        assertClassExists(PATH + "Event");
+    }
+}
