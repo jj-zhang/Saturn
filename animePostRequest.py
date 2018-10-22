e6a14108367bfdacf6b4e9e0504810d3b59cfc25
Prynciss Ng <prynciss.ng@mail.utoronto.ca>
2018-10-22 02:30:18 -0400

    Added GlobalEventManager and animePostRequest script

---
+++ animePostRequest.py
@@ -0,0 +1,32 @@
+import requests
+import json
+
+
+query = '''
+query ($id: Int) {
+  Media (id: $id, type: ANIME) {
+    id
+    startDate{
+      year
+      month
+      day
+    }
+    title {
+      romaji
+      english
+      native
+    }
+  }
+}
+'''
+variables = {
+    'id': 15125
+}
+url = 'https://graphql.anilist.co'
+response = requests.post(url, json={'query': query, 'variables': variables})
+
+jData = json.loads(response.content)
+
+print("The response contains {0} properties".format(len(jData)))
+print("\n")
+print jData
