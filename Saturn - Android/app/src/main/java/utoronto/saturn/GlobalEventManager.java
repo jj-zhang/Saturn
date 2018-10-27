16627a5b1a97feac78892b3339af9e5261bb754a
Prynciss Ng <prynciss.ng@mail.utoronto.ca>
2018-10-27 18:41:55 -0400

    add in connection stuff, animepostrequest, and globalEventManager

---
+++ Saturn - Android/app/src/main/java/utoronto/saturn/GlobalEventManager.java
@@ -0,0 +1,19 @@
+package utoronto.saturn;
+
+import java.util.List;
+public class GlobalEventManager extends EventManager {
+    public GlobalEventManager() {
+        super();
+    }
+    public List<Event> getPopular(){
+        //from database
+        return null;
+    }
+    public List<Event> getTrending(){
+        //from database
+        return null;
+    }
+    public List<Event> getSuggested(){
+        return null;
+    }
+}
