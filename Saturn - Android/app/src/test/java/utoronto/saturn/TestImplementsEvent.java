e4877999f88cbac596d877b80d329e221fb135d1
Reece Martin <capt.rm.fae@gmail.com>
2018-10-27 12:59:36 -0400

    Hotfixing Tims Test

--- Saturn - Android/app/src/test/java/utoronto/saturn/TestImplementsEvent.java
+++ Saturn - Android/app/src/test/java/utoronto/saturn/TestImplementsEvent.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestImplementsEvent {
-    private static final Event LE = new Event(null, null, null, 0, 0, null, 0);
+    private static final Event LE = new Event("h", "h", "h", 0, 0, null, 0);
     private static final LocalEventManager LEM = new LocalEventManager();
 
     private static void assertImplementsOrExtends(Object instObj, Class<?> obj) {
