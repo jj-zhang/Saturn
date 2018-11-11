1b98ee44de195825127b45c7aca94b6af8bc1216
Prynciss Ng <prynciss.ng@mail.utoronto.ca>
2018-11-11 09:31:57 -0500

    Fixed sql query of get popular etc works

--- Saturn - Android/app/src/main/java/utoronto/saturn/EventDatabase.java
+++ Saturn - Android/app/src/main/java/utoronto/saturn/EventDatabase.java
@@ -21,8 +21,8 @@ public class EventDatabase extends Database {
 
     public void addEvent(String name, String genre, String url, String date, boolean isGlobal) throws SQLException{
         String bool = "FALSE";
-       if(isGlobal)
-           bool = "TRUE";
+        if(isGlobal)
+            bool = "TRUE";
         Statement st = super.connection.createStatement();
         st.executeQuery("INSERT INTO events (id, name, type, isGlobal, url, date)" +
                 " VALUES (DEFAULT, '" + name + "','" + genre + "','" + bool + "', '" + url +"', '" + date + "')");
@@ -35,9 +35,47 @@ public class EventDatabase extends Database {
 
     public List<Event> getPopular() throws SQLException, MalformedURLException, ParseException{
         Statement st = super.connection.createStatement();
-        ResultSet rs = st.executeQuery("SELECT eventID, COUNT(*) FROM users GROUP BY eventId");
+        ResultSet rs = st.executeQuery("SELECT eventID, COUNT(*) AS count FROM users GROUP BY eventId ORDER BY count(*) DESC");
         ArrayList<Event> eventLst = new ArrayList<>();
-        ResultSetMetaData rsmd = rs.getMetaData();
+        while (rs.next()) {
+            int index = rs.getInt(1);
+            System.out.println(index);
+            eventLst.add(createEvent(index));
+            if(eventLst.size() > 5)
+                break;
+        }
+        rs.close();
+        st.close();
+
+        return eventLst;
+
+    }
+
+    public List<Event> getTrending() throws SQLException, MalformedURLException, ParseException{
+        Statement st = super.connection.createStatement();
+        ResultSet rs = st.executeQuery("SELECT id FROM events ORDER BY date ORDER BY count(*) DESC");
+        ArrayList<Event> eventLst = new ArrayList<>();
+        while (rs.next()) {
+            eventLst.add(createEvent(rs.getInt(1)));
+            if(eventLst.size() > 5)
+                break;
+        }
+        rs.close();
+        st.close();
+
+        return eventLst;
+
+    }
+
+    public List<Event> getSuggested() throws SQLException, MalformedURLException, ParseException{
+        Statement st = super.connection.createStatement();
+        ResultSet rs = st.executeQuery("SELECT type, COUNT(*) FROM events GROUP BY type ORDER BY count(*) DESC");
+        String type = "";
+        while (rs.next()) {
+            type = rs.getString(1);
+        }
+        rs = st.executeQuery("SELECT id FROM events WHERE type = '" + type + "'");
+        ArrayList<Event> eventLst = new ArrayList<>();
         while (rs.next()) {
             eventLst.add(createEvent(rs.getInt(1)));
             if(eventLst.size() > 5)
@@ -72,6 +110,7 @@ public class EventDatabase extends Database {
 
         //https://stackoverflow.com/questions/12473550/how-to-convert-a-string-date-to-long-millseconds
         SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.print(date);
         Date parseDate = f.parse(date);
         long milliseconds = parseDate.getTime();
 
