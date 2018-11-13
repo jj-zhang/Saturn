8bb9bd9ab647c16ab65b40f55127de4f1da0584b
ninjayoshi <tim470773@gmail.com>
2018-11-12 20:48:45 -0500

    Added start, end dates, and cover image to anime scraper.

--- animePOSTRequest.py
+++ animePOSTRequest.py
@@ -1,28 +1,40 @@
 import requests
 import json
- query = '''
-query ($id: Int) {
-  Media (id: $id, type: ANIME) {
+
+query = '''
+query {
+Media (search: "naruto", type: ANIME) {
     id
-    startDate{
-      year
-      month
-      day
-    }
     title {
-      romaji
-      english
-      native
+        english
+        romaji
+        native
     }
-  }
+    startDate{
+        year
+        month
+        day
+    }
+    endDate{
+        year
+        month
+        day
+    }
+    coverImage{
+        large
+        medium
+    }
+    isAdult
+    }
 }
 '''
 variables = {
     'id': 15125
 }
+
 url = 'https://graphql.anilist.co'
 response = requests.post(url, json={'query': query, 'variables': variables})
- jData = json.loads(response.content)
- print("The response contains {0} properties".format(len(jData)))
+jData = json.loads(response.content)
+print("The response contains {0} properties".format(len(jData)))
 print("\n")
 print jData
