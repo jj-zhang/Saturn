f22e76144c8b67c12bddd34f1b97f86213f29668
Ray Kwan <kwanlokto@gmail.com>
2018-11-19 23:21:16 -0500

    connection not working trying to fix.

--- Saturn - Android/app/src/main/java/utoronto/saturn/app/front_end/views/LoginActivity.java
+++ Saturn - Android/app/src/main/java/utoronto/saturn/app/front_end/views/LoginActivity.java
@@ -3,17 +3,21 @@ package utoronto.saturn.app.front_end.views;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 import utoronto.saturn.*;
 import utoronto.saturn.app.R;
@@ -31,9 +35,13 @@ public class LoginActivity extends AppCompatActivity {
 
         button = findViewById(R.id.login_activity_button);
         button.setOnClickListener(this::onButtonClickAction);
+
+        pgsqlcon pgcon = new pgsqlcon();
+        pgcon.execute();
     }
 
     private void onButtonClickAction(View v) {
+
         AutoCompleteTextView usernameView = findViewById(R.id.txt_username);
         Editable username = usernameView.getText();
         TextInputEditText passwordView = findViewById(R.id.txt_password);
@@ -62,5 +70,34 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra(Intent.EXTRA_EMAIL, user.getEmail());
         startActivity(intent);
+    }
+
+
+    private class pgsqlcon extends AsyncTask<Void, Void, Void> {
+
+        public pgsqlcon() {
+            super();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            Connection conn = null;
+            Statement st = null;
+
+            try {
+                //STEP 2: Register JDBC driver
+                Class.forName("org.postgresql.Driver");
+
+                //STEP 3: Open a connection
+                Log.d("myTag", "Connecting to database...");
+
+                conn = DriverManager.getConnection("jdbc:postgresql://tantor.db.elephantsql.com:5432/tjlevpcn"
+                        , "tjlevpcn", "SlQEEkbB5hwPHBQxbyrEziDv7w5ozmUu");
+            } catch (Exception e){
+                System.out.println(e.getStackTrace());
+            }
+            return null;
+        }
+
     }
 }
