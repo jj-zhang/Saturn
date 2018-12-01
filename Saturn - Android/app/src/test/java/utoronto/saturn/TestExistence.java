8d51a594972d4fc2059e35139b870a4af6f1d129
Reece Martin <capt.rm.fae@gmail.com>
2018-11-30 22:24:19 -0500

    Adding comments.

--- Saturn - Android/app/src/test/java/utoronto/saturn/TestExistence.java
+++ Saturn - Android/app/src/test/java/utoronto/saturn/TestExistence.java
@@ -14,46 +14,55 @@ public class TestExistence {
         }
     }
 
+    // Ensure Event Class Exists
     @Test
     public void checkEventExists() {
         assertClassExists(PATH + "Event");
     }
 
+    // Ensure User Class Exists
     @Test
     public void checkUserExists() {
         assertClassExists(PATH + "User");
     }
 
+    // Ensure Eventmanager Class Exists
     @Test
     public void checkEventManagerExists() {
         assertClassExists(PATH + "EventManager");
     }
 
+    // Ensure LocalEventManager Class Exists
     @Test
     public void checkLocalEventManagerExists() {
         assertClassExists(PATH + "LocalEventManager");
     }
 
+    // Ensure GlobalEventManager Class Exists
     @Test
     public void checkGlobalEventManagerExists() {
         assertClassExists(PATH + "GlobalEventManager");
     }
 
+    // Ensure Database Class Exists
     @Test
     public void checkDatabaseClassExists() {
         assertClassExists(PATH + "Database");
     }
 
+    // Ensure EventDatabase Class Exists
     @Test
     public void checkEventDatabaseClassExists() {
         assertClassExists(PATH + "EventDatabase");
     }
 
+    // Ensure UserDatabase Class Exists
     @Test
     public void checkUserDatabaseClassExists() {
         assertClassExists(PATH + "UserDatabase");
     }
 
+    // Ensure DatabaseUtilities Class Exists
     @Test
     public void checkDatabaseUtilitiesClassExists() {
         assertClassExists(PATH + "DatabaseUtilities");
